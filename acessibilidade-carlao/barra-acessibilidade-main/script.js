0e0364bf0bf81274e8e48af50181b8cc6e996a05
Joao Felipe Vicente <161844290+lfywho@users.noreply.github.com>
2024-09-12 08:42:50 -0300

    Add files via upload

---
+++ acessibilidade-carlao/barra-acessibilidade-main/script.js
@@ -0,0 +1,21 @@
+function acao1() {
+
+    document.getElementById('libras').style.display = "inline";
+    document.getElementById('modoclaro').style.display = "inline";
+    document.getElementById('lupa').style.display = "inline";
+    document.getElementById('modoescuro').style.display = "inline";
+    document.getElementById('acessibilidade1').style.display = "none";
+    document.getElementById('acessibilidade2').style.display = "inline";
+    document.getElementById('lupamenos').style.display = "inline";
+}
+
+function acao2() {
+
+    document.getElementById('libras').style.display = "none";
+    document.getElementById('modoclaro').style.display = "none";
+    document.getElementById('lupa').style.display = "none";
+    document.getElementById('modoescuro').style.display = "none";
+    document.getElementById('acessibilidade1').style.display = "inline";
+    document.getElementById('acessibilidade2').style.display = "none";
+    document.getElementById('lupamenos').style.display = "none";
+}
